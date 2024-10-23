155d9748358c0ac1a0e9bd089c9b32de1f166e96
preranna <PreranaKoirala2722@gmail.com>
2024-10-23 15:04:34 +0545

    Adding buy-now button

--- src/app/page.js
+++ src/app/page.js
@@ -19,17 +19,28 @@ export default function Home() {
     fetchProduct();
   }, []);
 
+  const handleBuyNow = async (productId) => {
+    const response = await axios.post(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/order/buy-now`,{
+      productId
+    });
+    alert('Product bought successfully');
+  }
+
   return (
     <div>
       <h1 className="font-bold text-center p-4">Products</h1>
-      <ul className="m-4">
+      <div className="m-4 flex flex-wrap gap-2">
         {ProductsList.map((product) => (
-          <li key={product._id}>
-           {product.name} - Stock: {product.stock}
-           <img src={`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/${product.imageURL}`}/>
-          </li>
+          <div key={product._id} className="flex flex-col border rounded p-2" >   
+           <img src={`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/${product.imageURL}`} className="w-72 aspect-square object-contain"/>
+           <h3 className="text-2xl">
+           {product.name} 
+           </h3>
+           <span className="text-xl">NPR. {product.price}</span>
+           <button className="bg-gray-300" onClick ={() => handleBuyNow(product._id)}>Buy Now</button>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
